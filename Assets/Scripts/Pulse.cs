7128cd807b5f50900683c52c13362db6b17ffe9a
Chao Chen <charliechen.bupt@gmail.com>
2015-09-18 08:31:05 -0700

    Add a ring pulsing effect
    It's in testScene.unity. It will generate a ring each 1.4 seconds.

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+
+	public Color colorStart = Color.red;
+	public Color colorEnd = Color.green;
+	public float duration = 1.0F;
+	public Renderer rend;
+	public float delay = 1.0F;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += new Vector3(0.3F, 0.3F, 0);
+
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		rend.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+		Object.Destroy(gameObject, 1.0f);
+	}
+}
---
+++ Assets/Scripts/RingSpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingSpawn : MonoBehaviour 
+{
+public GameObject ring;  
+public float fiveSecond = 0;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		fiveSecond += Time.deltaTime;
+		if (fiveSecond > 1)
+		{		
+			fiveSecond = 0;
+				GameObject obj = Instantiate (ring) as GameObject;  
+				obj.transform.position = new Vector3 (20, 0, 17);
+
+		}
+	}
+
+}
